ee5f14b96ad6e0d324ad96c50b02e7cc6c2e8e29
IlyaDenisov <91316379+IlyaDenisov@users.noreply.github.com>
2021-10-01 16:42:26 +0300

    Add files via upload

---
+++ ДЗ1.cpp
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    char a;
+    while ((a = getchar())
+        && a != '\n'
+        && a != '\0')
+    {
+        switch (a)
+        {
+        case '.':
+            printf(" | ");
+            break;
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            printf("%c", a);
+        case '-':
+        case ' ':
+            printf("");
+            break;
+        default:
+            printf("ERROR\n\n");
+        }
+    }
+    printf("\n");
+}
